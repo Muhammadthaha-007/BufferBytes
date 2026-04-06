4c237feb0773e367f96f0a4eb58d384ea4eb3095
Muhammad Thaha <thahakakkuni6@gmail.com>
2026-04-06 10:13:01 +0530

    updated-2

--- buffb1.py
+++ buffb1.py
@@ -20,22 +20,19 @@
 #             print(" ",end=" ")
 #     print()
 
-rows = 5
-print("--Play Button--")
-
-for row in range(rows*2-1):
-    for col in range(rows):
-        if row == 0 and col == 0 or\
-             row == 1 and col == 1 or\
-                 row == 2 and col in [0,2] or\
-                    row == 3 and col in [1,3] or\
-                         row == 4 and col in [0,2,4] or\
-                             row == 5 and col in [1,3] or\
-                                row == 6 and col in [0,2] or\
-                                     row == 7 and col == 1 or\
-                                         row == 8 and col == 0:
-                                         print("*",end=" ")
-        else:
+rows = int(input("Enter the row :"))
+temp_a = rows * 2 // 2
+n = temp_a
+for row in range(rows*2):
+    for col in range(row+1):
+        if row > temp_a and col == n:
             print(" ",end=" ")
-
+            n -= 1
+        elif row > temp_a and col > n:
+            print(" ",end="")
+        elif row % 2 == 0 and col % 2 != 0 or row % 2 != 0 and col % 2 == 0:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")  
     print()
+
