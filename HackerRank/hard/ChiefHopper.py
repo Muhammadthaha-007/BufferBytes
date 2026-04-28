9d32454fbffedf105a8d7f08340a8dfb5909e0c4
Muhammad Thaha <thahakakkuni6@gmail.com>
2026-04-28 14:32:25 +0530

    ChiefHopper - Done

--- HackerRank/hard/ChiefHopper.py
+++ HackerRank/hard/ChiefHopper.py
@@ -1,11 +1,23 @@
-def chiefHopper(arr):
-    for bot_energy in range(1,max(arr)+1):
-        result = [bot_energy,]
-        for height in arr:
-            if height != bot_energy:
-                bot_energy += (bot_energy-height)
-            result.append(bot_energy)
-        if not min(result) < 0:
-            return result[0]
 
-print(chiefHopper([2,3,4,3,2]))
+def chiefHopper(arr):
+    arr.reverse()
+    energy = 0
+    for height in arr:
+        energy = (energy + height+1)//2
+    return energy
+
+print(chiefHopper([1,6,4]))
+
+
+
+
+
+
+    # for bot_energy in range(1,max(arr)+1):
+    #     result = [bot_energy]
+    #     for height in arr:
+    #         if height != bot_energy:
+    #             bot_energy += (bot_energy-height)
+    #         result.append(bot_energy)
+    #     if not min(result) < 0:
+    #         return result[0]
